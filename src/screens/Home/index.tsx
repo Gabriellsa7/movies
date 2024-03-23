91f6006da6aea6edfa6ac34529e1515097d0e688
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-03-23 02:13:34 -0300

    Feat: I created the infinite scroll and search bar functionality.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,10 @@
-import { FlatList, Text, TextInput, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { styles } from "./styles";
 import { MagnifyingGlass } from "phosphor-react-native";
 import { useEffect, useState } from "react";
@@ -14,15 +20,53 @@ interface Movie {
 
 export function Home() {
   const [discoveryMovies, setDiscoveryMovies] = useState<Movie[]>([]);
+  const [searchResultMovies, setSearchResultMovies] = useState<Movie[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [noResult, setNoResult] = useState(false);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     loadMoreData();
   }, []);
 
   const loadMoreData = async () => {
-    const res = await api.get("/movie/popular");
-    setDiscoveryMovies(res.data.results);
+    setLoading(true);
+    const res = await api.get("/movie/popular", {
+      params: {
+        page,
+      },
+    });
+    setDiscoveryMovies([...discoveryMovies, ...res.data.results]);
+    setPage(page + 1);
+    setLoading(false);
   };
+
+  const searchMovies = async (query: string) => {
+    setLoading(true);
+    const res = await api.get("/search/movie", {
+      params: {
+        query,
+      },
+    });
+    if (res.data.results.length === 0) {
+      setNoResult(true);
+    } else {
+      setSearchResultMovies(res.data.results);
+    }
+    setLoading(true);
+  };
+
+  const handleSearch = (text: string) => {
+    setSearch(text);
+    if (text.length > 2) {
+      searchMovies(text);
+    } else {
+      setSearchResultMovies([]);
+    }
+  };
+
+  const movieData = search.length > 2 ? searchResultMovies : discoveryMovies;
 
   return (
     <View style={styles.container}>
@@ -33,13 +77,15 @@ export function Home() {
             placeholderTextColor="#FFF"
             placeholder="Search"
             style={styles.input}
+            value={search}
+            onChangeText={handleSearch}
           />
           <MagnifyingGlass color="#FFF" size={25} weight="light" />
         </View>
       </View>
       <View>
         <FlatList
-          data={discoveryMovies}
+          data={movieData}
           numColumns={3}
           renderItem={(item) => <CardMovies data={item.item} />}
           showsVerticalScrollIndicator={false}
@@ -47,7 +93,10 @@ export function Home() {
             padding: 35,
             paddingBottom: 100,
           }}
+          onEndReached={() => loadMoreData()}
+          onEndReachedThreshold={0.5}
         />
+        {loading && <ActivityIndicator size={50} color="#0296E5" />}
       </View>
     </View>
   );
